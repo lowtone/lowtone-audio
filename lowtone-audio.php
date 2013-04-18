a6bcee1010ddac413c62cdf8ef742a83b3edec0e
Paul van der Meijs <paul@lowtone.nl>
2013-04-18 19:51:01 +0200

    Updated for Lowtone Content plugin.

--- lowtone-audio.php
+++ lowtone-audio.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Plugin Name: Audio
- * Plugin URI: http://wordpress.lowtone.nl/audio
+ * Package Name: Audio
+ * Package URI: http://wordpress.lowtone.nl/audio
  * Description: Embed audio in Posts.
  * Version: 1.0
  * Author: Lowtone <info@lowtone.nl>
@@ -13,23 +13,33 @@
  * @copyright Copyright (c) 2011-2012, Paul van der Meijs
  * @license http://wordpress.lowtone.nl/license/
  * @version 1.0
- * @package wordpress\plugins\lowtone\audio
+ * @package wordpress\Packages\lowtone\audio
  */
 
 namespace lowtone\audio {
 
-	// Init Lowtone Util
+	use lowtone\content\packages\Package;
+
+	// Includes
 	
-	if (!include_once(WP_CONTENT_DIR . "/libs/lowtone/lowtone.php"))
-		trigger_error("Lowtone Util not found", E_USER_ERROR);
+	if (!include_once WP_Package_DIR . "/lowtone-content/lowtone-content.php") 
+		return trigger_error("Lowtone Content Package is required", E_USER_ERROR) && false;
 
-	// Register embed handler
+	Package::init(array(
+			Package::INIT_PACKAGES => array("lowtone", "lowtone\\wp"),
+			Package::INIT_MERGED_PATH => __NAMESPACE__,
+			Package::INIT_SUCCESS => function() {
 
-	wp_embed_register_handler("html5_audio", "#^https?://.+\.(mp3|ogg|wav)$#i", function($matches, $attr, $url, $rawattr) {
-		wp_enqueue_script("lowtone_audio", plugins_url("/assets/scripts/jquery.audio.js", __FILE__), array("audio", "modernizr"));
-		wp_enqueue_style("lowtone_audio", plugins_url("/assets/styles/audio.css", __FILE__));
+				// Register embed handler
 
-		return sprintf('<audio preload="auto"><source src="%1$s" /></audio>', sprintf($matches[0]));
-	});
+				wp_embed_register_handler("html5_audio", "#^https?://.+\.(mp3|ogg|wav)$#i", function($matches, $attr, $url, $rawattr) {
+					wp_enqueue_script("lowtone_audio", Packages_url("/assets/scripts/jquery.audio" . (!Util::isScriptDebug() ? "-min" : "") . ".js", __FILE__), array("audio", "modernizr"));
+					wp_enqueue_style("lowtone_audio", Packages_url("/assets/styles/audio.css", __FILE__));
+
+					return sprintf('<audio preload="auto"><source src="%1$s" /></audio>', sprintf($matches[0]));
+				});
+
+			}
+		));
 
 }
